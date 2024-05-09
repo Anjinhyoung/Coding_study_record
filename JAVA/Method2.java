b62f997c3fa35b5448267fa82f1d3624752b34ff
2월32일 <117788976+Anjinhyoung@users.noreply.github.com>
2024-05-09 16:50:03 +0900

    Update Method2.java

--- JAVA/Method2.java
+++ JAVA/Method2.java
@@ -1,11 +1,9 @@
-package day17;
-
 public class Method2 {
 	public static void main(String[] args) {
 		System.out.println(f());
 	}
 	static int f(){
-		System.out.println("f() ȣ��!");
+		System.out.println("f() È£Ãâ!");
 		return 100;
 	}
 
