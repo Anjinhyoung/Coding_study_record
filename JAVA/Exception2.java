3bb100332578da268d2902a9ca7daaa7ec360597
2월32일 <117788976+Anjinhyoung@users.noreply.github.com>
2024-05-09 17:56:15 +0900

    Update Exception2.java

--- JAVA/Exception2.java
+++ JAVA/Exception2.java
@@ -1,13 +1,18 @@
 public class Exception2 {
 	public static void main(String[] args) {
 		try {
-			Exception e=new Exception("°íÀÇ ¿¹¿Ü");
-			throw e;
+			int []a= {2,0};
+			int b=4;
+			int c=b/a[2];
 		}
-		catch(Exception e) {
-			System.out.println("¿¹¿Ü ¹ß»ý");
-			System.out.println(e.getMessage());
+		catch(ArithmeticException e) {
+		System.out.println("산술 오류 발생");
 		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("배열 길이 오류 발생");
+			}
+		System.out.println("예외 처리 공부 중");
 	}
 }
+
 
